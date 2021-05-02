a26d0d2508c61f3191647955953f7032ed529870
Jones <git@jonasscheiwiller.ch>
2021-05-02 17:22:21 +0200

    [Update] Still works

--- site/plugins/mh-image-block/index.php
+++ site/plugins/mh-image-block/index.php
@@ -3,36 +3,52 @@
 Kirby::plugin('jones-s/mh-image-block', [
   'hooks' => [
     'page.update:after' => function ($newPage, $oldPage) {
+
+      foreach ($newPage->content()->fields() as $field) {
         $newBlocks = new Kirby\Cms\Blocks();
-        $blocks    = $newPage->teaserText()->toBlocks();
+        // there is no method to get the fields type
+        // except by getting the pages blueprint and looking for the fields key
+        // https://forum.getkirby.com/t/how-to-determine-field-type-in-custom-field-method/19254
+        $fieldKey = $field->key(); // name of the field, like 'teasertext'
 
-        foreach ($blocks as $block) :
-        if ($block->type() !== 'image') {
-            $newBlocks->add($block);
-        } else {
-            $url      = $block->image()->toFile()->url();
-            $images   = $block->image()->toFiles()->pluck('filename');
-            $newBlock = new \Kirby\Cms\Block(
-                [
-              "content" => [
-                'location' => 'kirby',
-                'image'    => $images,
-                'alt'      => $block->alt()->value(),
-                'caption'  => $block->caption()->value(),
-                'url'      => $url,
-                'link'     => $block->link()->value(),
-              ],
-              'id'   => $block->id(),
-              'type' => $block->type()
-            ],
-            );
-            $newBlocks->add($newBlock);
+        // https://getkirby.com/docs/reference/objects/cms/page-blueprint/field
+        $fieldDefinition = $newPage->blueprint()->field($fieldKey);
+
+        if (isset($fieldDefinition['type'])) { // some field definitions dont have a type 🤷‍♂️
+
+          $blocks = $newPage->teaserText()->toBlocks();
+
+          foreach ($blocks as $block) {
+              if ($block->type() !== 'image') {
+                  $newBlocks->add($block);
+              } else {
+                  $url      = $block->image()->toFile()->url();
+                  $images   = $block->image()->toFiles()->pluck('filename');
+                  $newBlock = new \Kirby\Cms\Block(
+                      [
+                  "content" => [
+                    'location' => 'kirby',
+                    'image'    => $images,
+                    'alt'      => $block->alt()->value(),
+                    'caption'  => $url,
+                    // 'caption'  => $block->caption()->value(),
+                    'url'      => $url,
+                    'link'     => $block->link()->value(),
+                  ],
+                  'id'   => $block->id(),
+                  'type' => $block->type()
+                ],
+                  );
+                  $newBlocks->add($newBlock);
+              }
+          }
+
+          $newPage->update([
+            'teaserText' => $newBlocks->toArray(),
+          ]);
         }
-        endforeach;
 
-      $newPage->update([
-        'teaserText' => $newBlocks->toArray(),
-      ]);
+      }
     }
   ]
 ]);
