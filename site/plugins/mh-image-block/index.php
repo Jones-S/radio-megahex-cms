c2f25499ca0f58daee54637867f22d1408859e36
Jones <git@jonasscheiwiller.ch>
2021-05-02 15:26:30 +0200

    [Update] Image Blocks: Save absolute url to image block upon update

--- site/plugins/mh-image-block/index.php
+++ site/plugins/mh-image-block/index.php
@@ -1,92 +1,38 @@
 <?php
 
 Kirby::plugin('jones-s/mh-image-block', [
-  'blueprints' => [
-    'blocks/mhimage' => __DIR__ . '/blueprints/blocks/mhimage.yml',
-    'files/mhimage'  => __DIR__ . '/blueprints/files/mhblockimage.yml',
-  ],
   'hooks' => [
     'page.update:after' => function ($newPage, $oldPage) {
-      $blocks = $newPage->teaserText()->toBlocks();
+        $newBlocks = new Kirby\Cms\Blocks();
+        $blocks    = $newPage->teaserText()->toBlocks();
 
-      $mh = new Kirby\Cms\Block([
-        "content" => [
-          "image" => [
-            "screenshot-2021-04-14-at-20.41.20.png"
-          ],
-          "alt" => "alttext",
-          "caption" => "acse",
-          "link" => ""
-        ],
-        "type" => "mhimage"
+        foreach ($blocks as $block) :
+        if ($block->type() !== 'image' || !empty($block->image())) {
+            $newBlocks->add($block);
+        } else {
+            $url      = $block->image()->toFile()->url();
+            $images   = $block->image()->toFiles()->pluck('filename');
+            $newBlock = new \Kirby\Cms\Block(
+                [
+              "content" => [
+                'location' => 'kirby',
+                'image'    => $images,
+                'alt'      => $block->alt()->value(),
+                'caption'  => $block->caption()->value(),
+                'url'  => $url,
+                'link'     => $block->link()->value(),
+              ],
+              'id'   => $block->id(),
+              'type' => $block->type()
+            ],
+            );
+            $newBlocks->add($newBlock);
+        }
+        endforeach;
+
+      $newPage->update([
+        'teaserText' => $newBlocks->toArray(),
       ]);
-
-      // $mh = new Kirby\Cms\Block([
-      //   'content' => [
-      //     'text' => '<p>Hug them if you like. They might not appreciate it though.</p><p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum id ligula porta felis euismod semper. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>',
-      //   ],
-      //   'type' => 'text',
-      // ]);
-
-      $blocks = $blocks->add(new Kirby\Cms\Blocks([$mh]));
-
-      // $blocks = Kirby\Cms\Blocks::factory([
-      //   [
-      //     'content' => [
-      //       'text' => 'oo Nice heading'
-      //     ],
-      //     'type' => 'heading'
-      //   ],
-      //   [
-      //     "content" => [
-      //       "image" => [
-      //         "screenshot-2021-04-14-at-20.41.20.png"
-      //       ],
-      //       "alt" => "alttext",
-      //       "caption" => "acse",
-      //       "link" => ""
-      //     ],
-      //     "type" => "mhimage"
-      //   ],
-      //   [
-      //     'content' => [
-      //       'text' => 'oo This is some text'
-      //     ],
-      //     'type' => 'text'
-      //   ],
-      // ]);
-
-
-
-      // $newBlock = [
-      //   [
-      //     "content" => [
-      //       "image" => [
-      //         "screenshot-2021-04-14-at-20.41.20.png"
-      //       ],
-      //       "alt" => "alttext",
-      //       "caption" => "acse",
-      //       "link" => ""
-      //     ],
-      //     "type" => "mh-image"
-      //   ],
-      //   [
-      //     "content" => [
-      //       "text" => print_r($blocks, true)
-      //     ],
-      //     "type" => "text"
-      //   ],
-      // ];
-
-
-
-
-      $kirby = kirby();
-      $kirby->impersonate('kirby');
-
-      // $newPage->update([
-      //   'teaserText' => $blocks,
-      // ]);
     }
   ]
 ]);
